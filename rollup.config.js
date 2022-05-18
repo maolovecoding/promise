dc91e6e8ca366ae3135d6387c2a8618247449e6a
夜静听雨 <7710500+mao0826@user.noreply.gitee.com>
2022-05-18 10:31:22 +0800

    init project

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import tsPlugin from "rollup-plugin-typescript2";
+// rollup默认可以导出一个对象 作为打包的配置文件
+import resolve from "rollup-plugin-node-resolve";
+import path from "path";
+export default {
+  input: "./src/index.ts",
+  output: {
+    file: "./dist/bundle.js",
+    name: "MaoPromise",
+    format: "iife",
+    sourcemap: true,
+  },
+  plugins: [
+    tsPlugin({
+      tsconfig: path.resolve(__dirname, "./tsconfig.json"),
+    }),
+    resolve(),
+  ],
+};

9c611d26cea334c92ae49d02456eda71300904e5
夜静听雨 <7710500+mao0826@user.noreply.gitee.com>
2022-07-05 08:58:23 +0800

    race

--- src/promise.ts
+++ src/promise.ts
@@ -224,7 +224,13 @@ class Promise<T> {
       }
     });
   }
-  static race() {}
+  static race<T>(promises: Promise<T>[] | any[]) {
+    return new Promise((resolve, reject) => {
+      for (let i = 0; i < promises.length; i++) {
+        Promise.resolve(promises[i]).then(resolve, reject);
+      }
+    });
+  }
   get [Symbol.toStringTag]() {
     return "Promise";
   }
